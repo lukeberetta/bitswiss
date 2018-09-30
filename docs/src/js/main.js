ac85318ccb3924cc60e8158a346a4f06415a65bb
Luke Beretta <thelukeberetta@gmail.com>
2018-09-30 21:51:36 +0200

    widget logic

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -1,5 +1,4 @@
 // Menu
-
 let isOpen = false;
 
 function toggleMenu() {
@@ -23,43 +22,45 @@ function toggleMenu() {
   hero.classList.toggle("grow");
   heroGradient.classList.toggle("dark");
   // }, timeoutB);
-
-  // disableScroll();
 }
 
-function disableScroll() {
-  if (!isOpen) {
-    isOpen = true;
-    document.ontouchmove = function(e) {
-      e.preventDefault();
-    };
-  } else {
-    isOpen = false;
-    document.ontouchmove = function(e) {
-      return true;
-    };
-  }
+// Widget Config
+let slider = document.getElementById("myRange");
+let months = document.getElementById("months");
+let deposit = document.getElementById("deposit");
+let isChecked = document.getElementById("type");
+
+months.innerHTML = slider.value;
+slider.oninput = function() {
+  months.innerHTML = this.value;
+};
+
+// Update Chart
+function updateWidget() {
+  // return 3.62 for ETH or 6.11 for BTC
+  let yeild = (!isChecked.checked) ? 3.62 : 6.11;
+  // Test data
+  chart.data.datasets[0].data[6] = months.innerHTML * deposit.value + yeild;
+  // Update
+  chart.update();
 }
 
 // Chart.js
-
 let ctx = document.getElementById('widget').getContext('2d');
 let chart = new Chart(ctx, {
 
   // The type of chart
   type: 'line',
-
   // The dataset
   data: {
-    labels: ["M-1", "M-1", "M-1", "M-1", "M-1", "M-1", "M-1"],
+    labels: ["M-1", "M-2", "M-3", "M-4", "M-5", "M-6", "M-7"],
     datasets: [{
       label: false,
       backgroundColor: '#516bff80',
       borderColor: '#516bff',
-      data: [0, 10, 5, 2, 20, 30, 45],
+      data: [0, 10, 5, 2, 20, 30, months.innerHTML],
     }]
   },
-
   // Configuration
   options: {
     legend: {
